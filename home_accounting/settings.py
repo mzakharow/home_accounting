7380a6b38f54e3dc5e0c85acb6417076206728f7
mzakharow <mzakharov85@gmail.com>
2021-03-31 14:05:35 +0300

    settings.py

--- home_accounting/settings.py
+++ home_accounting/settings.py
@@ -74,8 +74,16 @@ AUTHENTICATION_BACKENDS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': '',
+    #     'USER': '',
+    #     'PASSWORD': '',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
 }
 
 
